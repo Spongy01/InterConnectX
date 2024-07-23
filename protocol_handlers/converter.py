4b2d26f8f2027686edae5fb2d19507d3d5a87fb7
Ali Asgar Padaria <aliasgarpadaria002@gmail.com>
2024-07-23 14:53:43 +0530

    Configured Rest to Soap to Rest pathway

--- protocol_handlers/converter.py
+++ protocol_handlers/converter.py
@@ -7,3 +7,7 @@ def parseSOAP(data):
     soap_dict = xmltodict.parse(data)
     
     return soap_dict
+
+def convertJSONtoSOAP(data):
+    json_dict = xmltodict.unparse(data)
+    return json_dict
--- protocol_handlers/soap_handler.py
+++ protocol_handlers/soap_handler.py
@@ -1,23 +1,51 @@
 from . import converter
 import json
+import requests
+import xmltodict
 
-def handle_soap_request(handler):
-    contentLength = int(handler.headers['Content-Length']) 
-    soap_data = handler.rfile.read(contentLength)
-    parsed_data = converter.parseSOAP(soap_data)
+def handle_soap_request(request):
+    if(request.headers['Content-Length'] is None):
+        contentLength = 0
+    else:
+        contentLength = int(request.headers['Content-Length']) 
+        
+    soap_data = request.rfile.read(contentLength)
+    parsed_data = ""
+    if(request.headers['Content-Length'] is not None):
+        print("Content Length : " + request.headers['Content-Length'])
+        parsed_data = converter.parseSOAP(soap_data)
     
-    # extract logic
+    # extract endpoint logic
+    if request.headers['Endpoint']:
+        print("Endpoint : " + request.headers['Endpoint'])
+    else:
+        print("Endpoint Not Provided")
+        request.send_response(200)
+        request.send_header('Content-type', 'text/xml')
+        request.end_headers()
+        
+        request.wfile.write(b'Endpoint header not provided')
+        return
+
     
-    # send to respective api gateway to handle request
-    
-    # receive api response
-    
+    # send to respective api gateway to handle request and handle responses
+    response = requests.get(url=request.headers['Endpoint'], data=parsed_data)
+    print("RESPONSE : ", response.json())
+
+    try:
+        json_response = response.json()
+        xml_response = xmltodict.unparse({"response": json_response}, pretty=True)
+    except ValueError:
+        xml_response = '<error>Invalid JSON response</error>'
     # parse back to JSON
+    # soap_response = converter.convertJSONtoSOAP(json_response)
+    print("SOAP response : " + xml_response)
     
     # send response
-    handler.send_response(200)
-    handler.send_header('Content-type', 'text/xml')
-    handler.end_headers()
+    request.send_response(response.status_code)
     
-    handler.wfile.write(json.dumps(parsed_data).encode('utf-8'))
+    request.send_header('Content-type', 'application/XML')
+    request.end_headers()
+    
+    request.wfile.write(xml_response.encode('utf-8'))
     return
